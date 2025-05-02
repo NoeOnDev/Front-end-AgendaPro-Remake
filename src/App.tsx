39f27d0eabe3501f8b0a1a52efb77fed4716f900
NoeOnDev <alxg5516@gmail.com>
2025-05-02 14:43:43 -0600

    refactor: eliminar formularios de autenticación y validación de campos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ResetPassword from "./pages/ResetPassword";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
         {/* Add other routes here */}
       </Routes>
     </BrowserRouter>
